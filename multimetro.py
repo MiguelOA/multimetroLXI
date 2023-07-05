ae36839628c56d15659f232563182de64cf6a583
alfaluis45 <73362377+alfaluis45@users.noreply.github.com>
2023-07-05 14:26:41 -0600

    version 0.5

--- multimetro.py
+++ multimetro.py
@@ -1,7 +1,7 @@
 # ****************************************************
 # MULTIMETRO VIRTUALIZADO LXI
 # DESARROLLADOR: Luis Alberto Barrera Sandoval
-# VERSION: 0.4
+# VERSION: 0.5
 # URL: https://github.com/MiguelOA/multimetroLXI
 # NOTAS
 # ****************************************************
@@ -18,29 +18,12 @@ from datetime import date
 
 # *********************************** GLOBAL VARIABLES
 idn = 'Multimeter: '
-c = 5   #number of connection attempts
 rm = pyvisa.ResourceManager()
 rm.list_resources()
 inst = None
-ipAdd = "10.0.9.23" #ip address
 timeout = 10000     #connection timeout
-instName = "TCPIP::" + ipAdd + "::INSTR"
+instName = ""
 print(instName)
-while(c > 0):
-    try:
-        inst = rm.open_resource(
-            instName, open_timeout=timeout)
-        inst.write("*CLS")
-        inst.query("*OPC?")
-        c = -1
-    except:
-        inst = None
-        c -= 1
-if(c == -1):
-    inst.timeout = timeout
-    idn += inst.query("*IDN?")
-    c
-print(c)
 x = None
 y = None
 killThread = False
@@ -51,28 +34,36 @@ playState = False
 Interval = 1.0
 fileSave = None
 Ent = None
-contData = 0
+contData = 10
 imaPlay = None
-
+lista = []
+flagConn = True
 # ****************************************** FUNCTIONS
 
 
-def fnInitConnection():
+def fnCheckConnection():
     rm = pyvisa.ResourceManager()
     rm.list_resources()
-    inst = rm.open_resource(instName)
-    idn = inst.query("*IDN?")
-    messagebox.showinfo(message=idn, title="Identification")
+    try:
+        inst = rm.open_resource(instName)
+        idn = inst.query("*IDN?")
+        messagebox.showinfo(message=idn, title="Identification")
+    except:
+        messagebox.showinfo(message='No connection', title="Error!")
+    
+
 
 
 def fnConfRes():
+    global x
+    global unit
     if(fileSave == None ):
         try:
             global pause
-            global unit
             pause = True
             while flag:
                 time.sleep(0.1)
+            time.sleep(1)
             inst.write("CONF:RES")
             unit = "Ohm"
             pause = False
@@ -175,7 +166,9 @@ def fnPlay():
             Sal = "Tiempo," + measure + "\n"
             fileSave.write(Sal)
             y = threading.Thread(target=fnRecordMeasure, daemon=True)
+            print('punto b')
             y.start()
+        print('punto c')
         global playState
         playState = not (playState)
         global imaPlay
@@ -186,7 +179,6 @@ def fnPlay():
             imaPlay = PhotoImage(file="./img/play.png")
             btnPlay.config(image=imaPlay)
         fnRecordState()
-    
 
 def fnSave():
     global fileSave
@@ -199,6 +191,18 @@ def fnSave():
         imaPlay = PhotoImage(file="./img/play.png")
         btnPlay.config(image=imaPlay)
     fnRecordState()
+
+def fnPlayMenu():
+    global playState
+    if(not(playState)):
+        fnPlay()
+
+def fnPauseMenu():
+    global playState
+    if(playState):
+        fnPlay()
+
+
 
 def fnGetConf():
     global pause
@@ -245,12 +249,13 @@ def fnRecordMeasure():
     global fileSave
     global playState
     global contData
+    global Ent
     print("Inicio")
-    while fileSave != None:
+    while fileSave != None and inst != None:
         if playState:
             now = datetime.now()
             currentTime = now.strftime("%H:%M:%S")
-            Sal = currentTime + "," + ENT + "\n"
+            Sal = currentTime + "," + Ent + "\n"
             if(OpcNumData.get()):
                 if(contData <= 0):
                     fnSave()
@@ -264,45 +269,251 @@ def fnRecordMeasure():
 
 
 def thread_Display():
+    global inst
+    global pause
+    global flag
+    global Ent
+    pause = False
     while not (killThread):
-        if not (pause):
-            global flag
-            flag = True
-            try:
-                global ENT
-                ENT = inst.query("READ?").replace("\n", "")
-                ENT += " " + unit
-                inst.query("*OPC?")
-                inst.write("*CLS")
-                if(not(pause)):
-                    lblDisplay.config(text=ENT.replace("\n", ""))
-            except:
-                if(not(pause)):
-                    lblDisplay.config(text="Not signal")
-                print("An exception occurred")
-            flag = False
+        flag = False
+        if(inst != None):
+            if not (pause):
+                flag = True
+                print('b')
+                try:
+                    Ent = inst.query("READ?").replace("\n", "")
+                    Ent += " " + unit
+                    inst.query("*OPC?")
+                    inst.write("*CLS")
+                    if not (pause):
+                        lblDisplay.config(text=Ent.replace("\n", ""))
+                except:
+                    
+                    if not (pause):
+                        lblDisplay.config(text="Not signal")
+                    print("An exception occurred")
+        else:
+            time.sleep(0.1)
+    print('Hilo finalizo')
+        
 
 def validationInt(input):
     if input.isdigit() and fileSave == None:
-        global Interval
-        Interval = int(input) /1000.0
-        lblInt.config(text="Interval in ms ("+ input +")")
-        print("Interval: "+ input)
+        n = int(input)
+        if(n > 0):
+            global Interval
+            Interval = int(input) /1000.0
+            lblInt.config(text="Interval in ms ("+ input +")")
+            print("Interval: "+ input)
     return True
 
 def validationData(input):
     if input.isdigit() and fileSave == None:
-        global contData
-        contData = int(input)
-        cheNumData.config(text="Numero de data ("+input+")")
+        n = int(input)
+        if(n > 0):
+            global contData
+            contData = int(input)
+            cheNumData.config(text="Numero de data ("+input+")")
     return True
+
+
+
+def fnInitConnection():
+    global rm
+    global inst
+    global instName
+    global idn
+    global unit
+    global pause
+    global flag
+    idn = 'Multimeter: '
+    c = 1   #number of connection attempts
+    pause = True
+    while flag:
+        time.sleep(0.1)
+    if(inst != None):
+        inst.close()
+    inst = None
+    while(c > 0):
+        try:
+            inst = rm.open_resource(
+                instName, open_timeout=timeout)
+            inst.write("*CLS")
+            inst.query("*OPC?")
+            c = -1
+        except:
+            c -= 1
+    if(c == -1):
+        inst.timeout = timeout
+        idn += inst.query("*IDN?")
+        try:
+            config = fnGetConf()
+            if(config.find("RES") >= 0):
+                unit = "OHM"
+            elif (config.find("CURR") >= 0):
+                unit = "A"
+            elif (config.find("VOLT") >= 0):
+                unit = "V"
+            else:
+                unit = "?"
+            on_closing()
+        except:
+            print("Error de conexion")
+        print(inst)
+        messagebox.showinfo(title='Info', message='The device is online')
+    else:
+        messagebox.showinfo(title='ERROR!', message='The device is not aviable')
+    window.title(idn)
+    pause = False
+
+def fnCloseConn():
+    global inst
+    global idn
+    global window
+    idn = 'Multimeter:'
+    if(inst != None):
+        inst.close
+    inst = None
+    window.title(idn)
+    pass
+
+def fnConnUSB():
+    global window_usb
+    name = cmbUSB.get()
+    if name != 'Seleciona...':
+        global instName
+        instName = name
+        fnInitConnection()
+    else:
+        messagebox.showinfo(message="Option no selected", title="Error!")
+    
+def fnConnLAN():
+    global instName
+    global entIP
+    global window_lan
+    ip = entIP.get()
+    octs = ip.split('.')
+    print(octs)
+    if(len(octs) == 4):
+        for oct in octs:
+            if(oct.isnumeric()):
+                if(int(oct) > 255):
+                    return False
+            else:
+                return False
+        ip = octs[0] + '.' + octs[1] + '.' + octs[2] + '.' + octs[3]
+        instName = "TCPIP::" + ip + "::INSTR"
+        fnInitConnection()
+    return False
+    
+
+def on_closing():
+    global flagConn
+    global window_usb
+    global window_lan
+    flagConn = True
+    if(window_usb != None):
+        window_usb.destroy()
+        window_usb = None
+    if(window_lan != None):
+        window_lan.destroy()
+        window_lan = None
+    print('Liberar ventana') 
+
+def fnUSBBtn():
+    global rm
+    global lista
+    global cmbUSB
+    global flagConn
+    global window_usb
+    if flagConn:
+        flagConn = False
+        lista = list(rm.list_resources())
+        window_usb = Toplevel()
+        window_usb.resizable(False,False)
+        window_usb.protocol("WM_DELETE_WINDOW", on_closing)
+        window_usb.geometry('500x500+10+10')
+        window_usb.title('USB connection')
+        #Create widget
+        cmbUSB = ttk.Combobox(window_usb, values=lista, width=30, state='readonly')
+        cmbUSB.set('Select...')
+        btn_conectar = Button(window_usb, text='Connect', command=fnConnUSB)
+        #Place widget
+        cmbUSB.place(x=20,y=20)
+        btn_conectar.place(x=20, y =100)
+
+
+def fnLANBtn():
+    global rm
+    global entIP
+    global flagConn
+    global window_lan
+    if flagConn:
+        flagConn = False
+        window_lan = Toplevel()
+        window_lan.resizable(False, False)
+        window_lan.protocol("WM_DELETE_WINDOW", on_closing)
+        window_lan.geometry('500x500+10+10')
+        window_lan.title('LAN connection')
+        #Create widgets
+        entIP = Entry(window_lan, width=15)
+        lblIP = Label(window_lan, text='IP Address')
+        btn_conectar = Button(window_lan, text='Connect', command=fnConnLAN)
+        #place widgets
+        entIP.place(x = 20, y = 20)
+        lblIP.place(x = 100, y = 20)
+        btn_conectar.place(x=20, y =100)
+
+
+def finalizar():
+    global pause
+    global killThread
+    global fileSave
+    global flag
+    global window
+    pause = True
+    while flag:
+        time.sleep(0.1)
+        print('Esperando...')
+        print(flag)
+    killThread = True
+    try:
+        inst.close()
+    except:
+        print('No hay instrumento')
+    if(fileSave != None):
+        fileSave.close()
+    window.destroy()
+    
 
 # ****************************************************
 # *********************************************** MAIN
 window = Tk()
+window.protocol("WM_DELETE_WINDOW", finalizar)
 window.geometry("1000x400+10+10")
 window.resizable(False, False)
 window.title(idn)
+print(pause)
+cmbUSB = None
+entIP = None
+window_usb = None
+window_lan = None
+
+my_menu = Menu(window)
+window.config(menu=my_menu)
+
+file_menu = Menu(my_menu)
+my_menu.add_cascade(label='Record', menu=file_menu)
+file_menu.add_command(label='Play', command=fnPlayMenu)
+file_menu.add_command(label='Pause', command=fnPauseMenu)
+file_menu.add_command(label='Save', command=fnSave)
+
+connection_menu = Menu(my_menu)
+my_menu.add_cascade(label='Connect', menu=connection_menu)
+connection_menu.add_command(label='USB connection', command=fnUSBBtn)
+connection_menu.add_command(label='LAN connection', command=fnLANBtn)
+connection_menu.add_command(label='Close connection', command=fnCloseConn)
+
 regInt = window.register(validationInt)
 regData = window.register(validationData)
 
@@ -312,17 +523,6 @@ imaSave = PhotoImage(file="./img/save.png")
 OpcNumData = IntVar()
 x = threading.Thread(target=thread_Display, daemon=True)
 
-
-try:
-    config = fnGetConf()
-    if(config.find("RES") >= 0):
-        unit = "OHM"
-    elif (config.find("CURR") >= 0):
-        unit = "A"
-    elif (config.find("VOLT") >= 0):
-        unit = "V"
-except:
-    print("Error de conexion")
 
 # ********************************* Widgets generation
 btnACCurrent = Button(window, text="AC Current",
@@ -341,7 +541,7 @@ btnResistance = Button(window, text="Resistance",
                        command=fnConfRes, width=15, 
     font=("Arial 12 bold"))
 btnConexion = Button(window, text="Check connection",
-                     command=fnInitConnection, width=15, 
+                     command=fnCheckConnection, width=15, 
     font=("Arial 12 bold"))
 lblDisplay = Label(
     window, text="Not signal", 
@@ -364,13 +564,14 @@ lblRecord = Label(window,
 lblInt = Label(window,
                text="Interval in ms ("+ str(int(Interval*1000))+")",
                font="Verdana 12")
-cheNumData = Checkbutton(window, text='Number of measurements (0)', 
+cheNumData = Checkbutton(window, text='Number of measurements (10)', 
                           variable=OpcNumData, command=fnConData,
                font="Verdana 12")
 entNumData = Entry(window, width=10,
                font="Verdana 12",
                validate='key',
                validatecommand=(regData, '%P'))
+
 
 
 # ********************************* Widget positioning
@@ -390,16 +591,7 @@ cheNumData.place(x=170, y=250)
 lblRecord.place(x=700, y=20)
 # ************************************** Threads start
 x.start()
+window.mainloop()
 
-if(c == -1):
-    window.mainloop()
-    pause = True
-    while flag:
-        time.sleep(0.1)
-    killThread = True
-    inst.close()
-killThread = True
-if(fileSave != None):
-    fileSave.close()
 # ****************************************************
 # ************************************ FINAL DE SCRIPT
